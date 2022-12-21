837130c0dd95416cdb7453ddbf7cafb54a4d7c99
rifat1210 <121188401+rifat1210@users.noreply.github.com>
2022-12-22 01:17:36 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,13 +23,13 @@ global.APIKeys = {
 
 //—————「 Setting Owner 」—————//
 global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6282127096310'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6282127096310' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = Rifat //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'RifatBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'RifatTeam' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
